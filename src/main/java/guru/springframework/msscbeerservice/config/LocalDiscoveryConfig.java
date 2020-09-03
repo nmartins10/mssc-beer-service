353af68410d095cda96bda588e72848e24ac453a
Nuno Martins <nuno.martins@externals.adidas.com>
2020-09-03 15:57:27 +0100

    stuff

---
+++ src/main/java/guru/springframework/msscbeerservice/config/LocalDiscoveryConfig.java
@@ -0,0 +1,14 @@
+package guru.springframework.msscbeerservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @author Nuno Martins
+ */
+@Profile("local-discovery")
+@EnableDiscoveryClient
+@Configuration
+public class LocalDiscoveryConfig {
+}
